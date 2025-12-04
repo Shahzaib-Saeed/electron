ebb1b7f0d31a0bf9b29dbfdd54637bf7abaa30a5
Shahzaib <apple@192.168.10.5>
2025-12-05 03:39:19 +0500

    Initial commit: Electron source code

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, 'icon.png'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Load your hosted PHP app
+  win.loadURL("https://asoos.co.uk");
+
+  // Disable DevTools
+  win.webContents.on("before-input-event", (event, input) => {
+    if (input.control && input.key.toLowerCase() === "i") {
+      event.preventDefault();
+    }
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
